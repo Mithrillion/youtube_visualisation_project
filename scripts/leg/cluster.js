8d837f33459c0c7679067203af850295a0ecb177
mithrillion <aeroumbria@gmail.com>
2017-05-19 21:52:16 +1000

    finalising, need some style improvements

--- scripts/leg/cluster.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Peter on 10/05/2017.
- */
-d3.xml("data/clusters/hsbm-small.svg").mimeType("image/svg+xml").get(function(error, xml) {
-    if (error) throw error;
-    xml.documentElement.setAttribute("width", "100%");
-    xml.documentElement.setAttribute("height", "100%");
-    document.getElementById("clusters_plot").appendChild(xml.documentElement);
-});
